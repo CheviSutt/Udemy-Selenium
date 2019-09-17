f8fea891a1f03acbe8e951bc4c60896b7e7921bb
Chevi Sutton <chevi@redskytech.io>
2019-09-16 22:27:28 -0600

    Part 1 done

--- tests/contactUsTest_POM.js
+++ tests/contactUsTest_POM.js
@@ -1,3 +1,5 @@
+
+
 let request = require('sync-request');
 
 beforeEach(function () {
@@ -52,9 +54,7 @@ describe('Test Contact Us form WebdriverUni', function () {
     }
 
     contactUsDetails.forEach(function (contactDetail) {
-
         it('Should be able to submit a successful submission via contact us form', function (done) {
-
             setFirstName('Joe');
             setLastName('Blogs');
             setEmailAddress(contactDetail.email);
@@ -62,34 +62,28 @@ describe('Test Contact Us form WebdriverUni', function () {
             clickSubmitButton();
             confirmSuccessfulSumission();
         });
-
-        it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-            setFirstName('Mike');
-            setLastName('Woods');
-            setEmailAddress('mike_woods@mail.com');
-            clickSubmitButton();
-            confirmUnsuccessfulSumission();
-        });
-
-
-        it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-            browser.setValue("[name='first_name']", 'Sarah');
-            browser.setValue("[name='email']", 'sarah_woods@mail.com');
-            browser.click("[type='submit']");
-
-            var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
-            expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.false;
-        });
-
-        it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-            browser.setValue("[name='first_name']", 'Jim');
-            browser.setValue("[name='last_name']", 'Jomes');
-            browser.click("[type='submit']");
-
-            var errorText = browser.getText('body');
-            expect(errorText).to.include('Error: all fields are required');
-
-            var errorText = browser.isVisible('body');
-            expect(errorText, 'Error message is not visible').to.be.true;
-        });
     });
+
+    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+        setFirstName('Mike');
+        setLastName('Woods');
+        setEmailAddress('mike_woods@mail.com');
+        clickSubmitButton();
+        confirmUnsuccessfulSumission();
+    });
+
+
+    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+        setFirstName('Sarah');
+        setEmailAddress('sarah_woods@mail.com');
+        clickSubmitButton();
+        confirmUnsuccessfulSumission();
+    });
+
+    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+        setLastName('Jones');
+        setEmailAddress('jim_jones@mail.com');
+        clickSubmitButton();
+        confirmUnsuccessfulSumission();
+    });
+});
